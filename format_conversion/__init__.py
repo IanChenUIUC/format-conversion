d89bf7d0e0713ecb4c9dc1d795925e5c63f44172
Ian <ianchen3@illinois.edu>
2026-06-18 13:39:30 -0500

    export output type

--- format_conversion/__init__.py
+++ format_conversion/__init__.py
@@ -1,4 +1,4 @@
-from .convert import convert, ConvertOptions
+from .convert import convert, ConvertOptions, OutputType
 from .partition import partition
 
-__all__ = ["partition", "convert", "ConvertOptions"]
+__all__ = ["partition", "convert", "ConvertOptions", "OutputType"]
